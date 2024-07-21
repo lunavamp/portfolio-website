50aa5931da328eb67b67990165d6945f4ed72e65
Karina Kolesnichenko <blankfaces@yahoo.com>
2024-07-21 15:27:59 +0200

    working on contact modal

--- src/js/index.js
+++ src/js/index.js
@@ -55,3 +55,22 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   }).mount();
 });
+
+//modal
+const modal = document.getElementById("modal");
+const btn = document.querySelector(".openModal");
+const span = document.getElementById("closeModal");
+
+btn.onclick = function () {
+  modal.classList.add("show");
+};
+
+span.onclick = function () {
+  modal.classList.remove("show");
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.classList.remove("show");
+  }
+};
